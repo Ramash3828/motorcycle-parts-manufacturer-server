2d0c01346f188b2297a3bbf47d9a72c8579e672d
Ramash3828 <ramash3828@gmail.com>
2022-05-23 11:33:43 +0600

    user create done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -18,12 +19,40 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-        const productcollection = client
+        const productCollection = client
             .db("parts-manufacturer")
             .collection("products");
-
+        const userCollection = client
+            .db("parts-manufacturer")
+            .collection("users");
+        // User create and update in database
+        app.put("/user/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            console.log(email);
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(
+                filter,
+                updateDoc,
+                options
+            );
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
+                expiresIn: "1h",
+            });
+            res.send({ result, token });
+        });
+        // Add Products
+        app.post("/add-product", async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        });
         app.get("/add-product", async (req, res) => {
-            const result = await productcollection.find({}).toArray();
+            const result = await productCollection.find({}).toArray();
             res.send(result);
         });
     } finally {
